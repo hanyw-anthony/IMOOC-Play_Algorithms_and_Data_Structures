7c94425c86063612b76d54c40e3eec281ae8fdbd
Anthony.YW.Han <hanyw_devel@163.com>
2024-02-22 21:37:56 +0800

    阶段一/第2章/第4节 实现泛型线性查找算法
    - 在LinearSearch类中定义search()函数实现泛型线性查找算法
    - 在main()函数中增加相应测试

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -2,6 +2,12 @@ public class LinearSearch {
 
     private LinearSearch() {}
 
+    /**
+     * int版线性查找
+     * @param data 目标数组
+     * @param target 目标元素
+     * @return 返回目标元素的索引；若不存在，返回-1
+     */
     public static int searchInt(int[] data, int target) {
         for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
@@ -11,7 +17,23 @@ public class LinearSearch {
         return -1;
     }
 
+    /**
+     * 泛型版线性查找
+     * @param data 目标数组
+     * @param target 目标元素
+     * @return 返回目标元素的索引；若不存在，返回-1
+     */
+    public static <E> int search(E[] data, E target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
+        // int版测试
         int[] data = { 24, 18, 12, 9, 16, 66, 43, 4 };
 
         int res1 = LinearSearch.searchInt(data, 16);
@@ -19,5 +41,15 @@ public class LinearSearch {
 
         int res2 = LinearSearch.searchInt(data, 666);
         System.out.println(res2);
+
+        // 泛型版测试
+        // 使用Integer测试
+        Integer[] integers = { 24, 18, 12, 9, 16, 66, 43, 4 };
+
+        int res3 = LinearSearch.search(integers, 16);
+        System.out.println(res3);
+
+        int res4 = LinearSearch.search(integers, 666);
+        System.out.println(res4);
     }
 }
